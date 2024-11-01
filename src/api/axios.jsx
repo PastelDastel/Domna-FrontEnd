0ab5eba44059d504d1690c60eeb45f566646d898
Pastel Dastel <pastel.dastel@gmail.com>
2024-11-02 00:29:34 +0100

    Deploy works?

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = 'http://localhost:3500';
+const BASE_URL = 'https://1edf17b2-a202-47d1-94db-4087c4ce79af.eu-central-1.cloud.genez.io';
 
 export default axios.create({
     baseURL: BASE_URL
