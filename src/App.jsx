b973a46d409339adf7194d3822aea91f5ceedb03
Pastel Dastel <pastel.dastel@gmail.com>
2024-11-16 01:14:57 +0100

    fixed lag

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Register from "./components/Register/Register";
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import Layout from "./components/Global Components/Layout";
-import Editor from "./components/Editor/Editor";
 import Admin from "./components/Admin/Admin";
 import Missing from "./components/Missing/Missing";
 import Unauthorized from "./components/Unauthorized/Unauthorized";
@@ -14,15 +13,12 @@ import Courses from "./components/Courses/Courses";
 import About from "./components/About/About";
 import Blog from "./components/Blog/Blog";
 import ShoppingCart from "./components/ShoppingCart/ShoppingCart";
-const ROLES = {
-  User: "User",
-  Editor: "Editor",
-  Admin: "Admin",
-};
 // every 30s make a get / to keep the backend online
 setInterval(() => {
-  fetch("http://localhost:3500/")
-    .then()
+  fetch("https://domna-api.onrender.com")
+    .then(
+      console.log("pinged backend to keep it online")
+    )
 }, 30000);
 
 
@@ -42,7 +38,7 @@ function App() {
 
           <Route element={<RequireAuth allowedRoles={[4934503821911649, 6792941695628669]} />}>
             <Route path="/profile/:id" element={<Profile />} />
-            <Route path="/shopping-cart" element={<ShoppingCart/>} />
+            <Route path="/shopping-cart" element={<ShoppingCart />} />
           </Route>
           <Route element={<RequireAuth allowedRoles={[6792941695628669]} />}>
             <Route path="dashboard" element={<Admin />} />
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,23 +1,26 @@
-import Hero from "./Hero/Hero";
+import React, { Suspense } from "react";
 import style from "./About.module.css";
-import FirstSection from "./FirstSection/FirstSection";
-import SecondSection from "./SecondSection/SecondSection";
+
+const Hero = React.lazy(() => import("./Hero/Hero"));
+const FirstSection = React.lazy(() => import("./FirstSection/FirstSection"));
+const SecondSection = React.lazy(() => import("./SecondSection/SecondSection"));
+
 const About = () => {
   return (
-    <>
-      <div className={style.pageContainer}>
+    <div className={style.pageContainer}>
+      <Suspense fallback={<div>Loading...</div>}>
         <Hero text={"Conosci Marcella"} />
-        <div className={style.separatorHrFromHero}></div>
+      </Suspense>
+      <div className={style.separatorHrFromHero}></div>
+      <Suspense fallback={<div>Loading...</div>}>
         <FirstSection />
-        <div className={style.separatorHr}>
-          <hr className={style.separatorMind} />
-        </div>
-       {  <SecondSection /> }
-        <div className={style.separatorHrSecond}>
-          <hr className={style.separatorMindSecond} />
-        </div>
-      </div>
-    </>
+      </Suspense>
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <SecondSection />
+      </Suspense>
+
+    </div>
   );
 };
 
