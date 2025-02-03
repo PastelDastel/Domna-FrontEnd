c55de6eed4d79687a2b5a7080de9ae8b497b43d9
Pastel Dastel <pastel.dastel@gmail.com>
2025-02-03 13:56:06 +0100

    Added newsletter infograb

--- src/components/Home/Contact Section/ContactSection.jsx
+++ src/components/Home/Contact Section/ContactSection.jsx
@@ -1,24 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import style from "./ContactSection.module.css";
-import axios from "axios";
+import useAxiosPrivate from "../../../hooks/useAxiosPrivate";
 const ContactSection = () => {
-
+  const axiosPrivate = useAxiosPrivate();
   const submitForm = (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
     const data = {
-      name: formData.get("name"),
-      surname: formData.get("surname"),
+      nome: formData.get("name"),
+      cognome: formData.get("surname"),
       email: formData.get("email"),
       phone: formData.get("phone"),
     };
     try {
-      const res = axios.post("https://domna-backend-1.onrender.com/newsletter", data);
-      console.log(res);
-      return;
+
+      const submit = async () => {
+        console.log(data);
+        const res = await axiosPrivate.post("/newsletter", data);
+        console.log(res);
+        alert("Iscrizione avvenuta con successo!");
+        e.target.reset();
+      };
+      submit();
     } catch (err) {
       console.log(err);
+      alert("Errore nell'iscrizione alla newsletter");
     }
   };
   return (
@@ -27,16 +34,16 @@ const ContactSection = () => {
         <h2 className={style.contactTitle}>Ricevi La Newsletter Del Sito</h2>
         <p className={style.contactSubtitle}>Resta sempre aggiornata!</p>
 
-        <form className={style.contactForm}>
+        <form className={style.contactForm} onSubmit={submitForm}>
           <div className={style.inputGroup}>
             <div className={style.nameGroup}>
-              <input type="text" placeholder="Nome" required />
-              <input type="text" placeholder="Cognome" required />
+              <input type="text" placeholder="Nome" required name="name"/>
+              <input type="text" placeholder="Cognome" required name="surname"/>
 
             </div>
 
-            <input type="email" placeholder="Email" required />
-            <input type="text" placeholder="Telefono" required />
+            <input type="email" placeholder="Email" required name="email"/>
+            <input type="text" placeholder="Telefono" required name="phone"/>
           </div>
           <button
             type="submit"
